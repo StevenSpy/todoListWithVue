dde14f05109eefcbe03f6a623bf93c72ff8dfc4e
Steven <spysschaert.steven0@gmail.com>
2019-10-07 15:21:14 +0200

    Change the input form bulma to another one

--- todolistvue/src/components/TodoInputText.vue
+++ todolistvue/src/components/TodoInputText.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="control">
-    <input class="input" type="text" placeholder="Enter your tasks : " v-bind="value" />
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-medium" type="text" placeholder="Enter your task" />
+    <span class="icon is-left">
+      <i class="fas fa-check"></i>
+    </span>
   </div>
 </template>
 
