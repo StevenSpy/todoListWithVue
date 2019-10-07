7fb61a1630b1089023c80aeb0994a27299b32b38
Steven <spysschaert.steven0@gmail.com>
2019-10-07 14:43:29 +0200

    unordered list created in TodoListItem.vue and link that component into TodoList.vue

--- todolistvue/src/components/TodoList.vue
+++ todolistvue/src/components/TodoList.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <TodoInputText />
+    <TodoListItem :todos="todos" />
   </div>
 </template>
 
 <script>
 import TodoInputText from "@/components/TodoInputText.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
 export default {
   components: {
-    TodoInputText
+    TodoInputText,
+    TodoListItem
   },
 
   data() {
