b5a6d0835247e687a448c7dc1911c4fae4b543e7
Steven <spysschaert.steven0@gmail.com>
2019-10-07 14:18:25 +0200

    Input from bulma added in the component TodoInputText.vue

--- todolistvue/src/components/TodoInputText.vue
+++ todolistvue/src/components/TodoInputText.vue
@@ -1,4 +1,8 @@
-<template></template>
+<template>
+  <div class="control">
+    <input class="input" type="text" placeholder="Enter your tasks : " />
+  </div>
+</template>
 
 <script>
 export default {};
